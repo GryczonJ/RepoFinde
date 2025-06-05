4ee45a9d1f5592f8da39bd439e87dff0ac28519f
GryczonJ <jan.gryczon1@gmail.com>
2025-06-05 23:42:26 +0200

    Update composer.json and composer.lock for dependency adjustments; modify HomeController to use Symfony routing attributes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,11 +1,15 @@
 <?php
+
 namespace App\Controller;
 
+use Symfony\Component\Routing\Attribute\Route;
+//use Symfony\Component\HttpFoundation\Response;
 
 class HomeController
 {
+    #[Route('/', name: 'home')]
     public function index()
     {
-        return 'Hello, World!';
+        echo "Hello, World!";
     }
 }
